c16bdc3f71aa2692f3314e830ab3b09be43cfb15
fahri-r <fahrim42@protonmail.com>
2021-01-04 13:25:14 +0700

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace asp.netcore_perpustakaan.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
