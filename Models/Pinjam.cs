fc2a1c760e9f87eddb0707d011e7433064404a8d
fahri-r <fahrim42@protonmail.com>
2021-01-26 23:07:39 +0700

    Added Create and Delete for Peminjaman

--- Models/Pinjam.cs
+++ Models/Pinjam.cs
@@ -1,21 +1,28 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
-
-// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
-// If you have enabled NRTs for your project, then un-comment the following line:
-// #nullable disable
 
 namespace Perpustakaan.Models
 {
     public partial class Pinjam
     {
         public int Id { get; set; }
+        [DataType(DataType.Date)]
         public DateTime TglPinjam { get; set; }
+        [DataType(DataType.Date)]
         public DateTime? TglKembali { get; set; }
         public string Kembali { get; set; }
         public string KodeBuku1 { get; set; }
         public string Nip1 { get; set; }
         public string NoKtp1 { get; set; }
+
+        [NotMapped]
+        public List<Anggota> anggotaCollection {get; set;}
+        [NotMapped]
+        public List<Buku> bukuCollection {get; set;}
+        [NotMapped]
+        public List<Pegawai> pegawaiCollection {get; set;}
 
         public virtual Buku KodeBuku1Navigation { get; set; }
         public virtual Pegawai Nip1Navigation { get; set; }
